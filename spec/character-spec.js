4f8eccf6d20463b0504634993a26b016146c9924
Stuart Gill <stuart.a.gill@gmail.com>
2019-01-11 16:27:05 -0800

    add enough tests to cover all current birthday methods

--- spec/character-spec.js
+++ spec/character-spec.js
@@ -6,14 +6,9 @@ import { Birthday } from './../src/birthday';
 
 describe('Birthday', function() {
 
-  // afterEach(function() {
-  //   userBirthday= undefined;
-  // });    
-
   it('should return one year when inputted one year in miliseconds', function(){
     const oneYear = 31556952000;
-    const result = Birthday.milisecondsToYears(oneYear);
-    expect(result).toBe(1);
+    expect(Birthday.milisecondsToYears(oneYear)).toBe(1);
   });
 
   it('should return one year when asked age difference between now and a year from now, margin of error 1 day', function() {
@@ -22,9 +17,27 @@ describe('Birthday', function() {
     const month = today.getMonth();
     const day = today.getDate();
     const userBirthday = new Birthday(year-1, month+1, day);
-    const roughAge = Birthday.getAge(userBirthday);
-    expect(roughAge - 31556952000).toBeLessThan(8.64e+7);
+    expect(Birthday.getAge(userBirthday) - 31556952000).toBeLessThan(8.64e+7);
   });
 
+  it('should return about 4.16 when one year is entered', function(){
+    expect(Birthday.getMercuryAge(1)).toBeCloseTo(4.16,1);
+  });
 
-});
+  it('should return about 1.61 when one year is entered', function(){
+    expect(Birthday.getVenusAge(1)).toBeCloseTo(1.61,1);
+  });
+
+  it('should return about .53 when one year is entered', function(){
+    expect(Birthday.getMarsAge(1)).toBeCloseTo(.53,1);
+  });
+
+  it('should return about .08 when one year is entered', function(){
+    expect(Birthday.getJupiterAge(1)).toBeCloseTo(.08,1);
+  });
+
+  it('should return zero years left to live', function(){
+    expect(Birthday.yearsLeft(31556952000, 1)).toEqual(0);
+  });
+
+  });
--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-//import './styles.css';
+
 import $ from 'jquery';
-//import 'bootstrap';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import { Birthday } from './birthday';
 
 $(document).ready(function() {
-    // let char;
-    // let monster;
-    // let charClass;
+
     $("#birthdayForm").submit(function(event) {
         event.preventDefault();
         const userBirthdate = parseInt($('#birthdate').val());
@@ -19,27 +15,40 @@ $(document).ready(function() {
         
         let age = Birthday.getAge(userBirthday);
         let earthAge = Birthday.milisecondsToYears(age);
-        let mercuryAge = Birthday.milisecondsToYears(Birthday.getMercuryAge(userBirthday));
-        let venusAge = Birthday.milisecondsToYears(Birthday.getVenusAge(userBirthday));
-        let marsAge = Birthday.milisecondsToYears(Birthday.getMarsAge(userBirthday));
-        let jupiterAge = Birthday.milisecondsToYears(Birthday.getJupiterAge(userBirthday));
+        let mercuryAge = Birthday.milisecondsToYears(Birthday.getMercuryAge(age));
+        let venusAge = Birthday.milisecondsToYears(Birthday.getVenusAge(age));
+        let marsAge = Birthday.milisecondsToYears(Birthday.getMarsAge(age));
+        let jupiterAge = Birthday.milisecondsToYears(Birthday.getJupiterAge(age));
         let yearsLeft = Birthday.milisecondsToYears(Birthday.yearsLeft(age, userExpectancy));
         let mercuryYearsLeft = Math.floor(Birthday.milisecondsToYears(Birthday.yearsLeft(age, userExpectancy))/.24);
         let venusYearsLeft = Math.floor(Birthday.milisecondsToYears(Birthday.yearsLeft(age, userExpectancy))/.62);
         let marsYearsLeft = Math.floor(Birthday.milisecondsToYears(Birthday.yearsLeft(age, userExpectancy))/1.88);
         let jupiterYearsLeft = Math.floor(Birthday.milisecondsToYears(Birthday.yearsLeft(age, userExpectancy))/11.86);
 
-        //let earthAge = userBirthday.getAge;
-        console.log(earthAge);
-        console.log(mercuryAge);
-        console.log(venusAge);
-        console.log(marsAge);
-        console.log(jupiterAge);
-        console.log(yearsLeft);
-        console.log(mercuryYearsLeft);
-        console.log(venusYearsLeft);
-        console.log(marsYearsLeft);
-        console.log(jupiterYearsLeft);
-        console.log(new Date);
+        console.log(`your earth age is ${earthAge}`);
+        console.log(`your mercury age is ${mercuryAge}`);
+        console.log(`your venus age is ${venusAge}`);
+        console.log(`your mars age is ${marsAge}`);
+        console.log(`your jupiter age is ${jupiterAge}`);
+        
+        if (yearsLeft>0) {
+        console.log(`you have approximately ${yearsLeft} earth years left to live`);
+        console.log(`you have approximately ${mercuryYearsLeft} mercury years left to live`);
+        console.log(`you have approximately ${venusYearsLeft} venus years left to live`);
+        console.log(`you have approximately ${marsYearsLeft} mars years left to live`);
+        console.log(`you have approximately ${jupiterYearsLeft} jupiter years left to live`);
+        }
+        else {
+        yearsLeft= -yearsLeft;
+        mercuryYearsLeft= -mercuryYearsLeft;
+        venusYearsLeft= -venusYearsLeft;
+        marsYearsLeft = -marsYearsLeft;
+        jupiterYearsLeft = -jupiterYearsLeft;
+        console.log(`you have lived ${yearsLeft} earth years beyond your expected life span`);
+        console.log(`you have lived ${mercuryYearsLeft} mercury years beyond your expected life span`);
+        console.log(`you have lived ${venusYearsLeft} venus years beyond your expected life span`);
+        console.log(`you have lived ${marsYearsLeft} mars years beyond your expected life span`);
+        console.log(`you have lived ${jupiterYearsLeft} jupiter years beyond your expected life span`);
+        }
     });        
 });
